f80c792a5ec64f7e09a950ea73c33b3d47b28015
Fetivol <Fetisowova@gmail.com>
2023-09-19 20:09:39 +0200

    top movies

--- src/Api.js
+++ src/Api.js
@@ -1,1 +1,53 @@
-// import axios from 'axios';
+import axios from 'axios';
+
+// const BASE_URL = 'https://api.themoviedb.org/3';
+// const API_KEY = '31e4aa16d394f964079920d6173ce10a';
+// export async function fetchData(endpoint) {
+//   try {
+//     const response = await axios.get(`${BASE_URL}/${endpoint}`, {
+//       params: {
+//         api_key: API_KEY,
+//         language: 'en-US',
+//       },
+//     });
+
+//     return await response.data;
+//   } catch (error) {
+//     throw error;
+//   }
+// }
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = '31e4aa16d394f964079920d6173ce10a';
+
+export async function fetchData(endpoint, queryParams) {
+  const params = new URLSearchParams({
+    api_key: API_KEY,
+    language: 'en-US',
+    ...queryParams, // Additional query parameters
+  });
+
+  return await axios
+    .get(`${BASE_URL}/${endpoint}?${params.toString()}`)
+    .then(async response => {
+      if (response.status !== 200) {
+        throw new Error(response.status);
+      }
+      return response.data;
+    });
+}
+
+const queryParams = {
+  page: 1,
+  someOtherParam: 'value',
+};
+
+const endpoint = 'trending/movie/day'; // Your desired endpoint
+
+fetchData(endpoint, queryParams)
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,38 @@
+import { fetchData } from 'Api';
+
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+
 const Home = () => {
+  //   console.log(fetchTopMovies);
+  const [topMovies, setTopMovies] = useState([]);
+  useEffect(() => {
+    const endpoint = 'trending/movie/day';
+    async function getTopMovies() {
+      try {
+        const topMoviesList = await fetchData(endpoint);
+        console.log(topMoviesList);
+        setTopMovies(topMoviesList.results);
+      } catch (error) {
+        console.error(error);
+      } finally {
+      }
+    }
+    getTopMovies();
+  }, []);
   return (
     <div>
-      Home page
-      <br />
-      <span>Top films</span>
+      Trending Today
+      <ul>
+        {topMovies.map(({ id, title }) => {
+          return (
+            <li key={id}>
+              <Link to={`movies/${id}`}>{title}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
